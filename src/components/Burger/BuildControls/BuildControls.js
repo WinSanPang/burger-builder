322139bd7694d1be341518f9bfeaedd290c3bcca
Win San Pang <winsanpang@hotmail.co.uk>
2020-04-08 15:23:15 +0100

    Add remaining Burger Builder code

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Meat', type: 'meat'},
+    { label: 'Cheese', type: 'cheese'},
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Price: <strong>£{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)} 
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+                />
+        ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchaseable}
+            onClick={props.ordered}
+            >{props.isAuth ? 'Place Order' : 'Please Sign In'}</button>
+    </div>
+);
+
+export default buildControls;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.css';
+
+const logo = (props) => (
+    <div className={classes.Logo} style={{height: props.height}}>
+        <a href="/"><img 
+            src={burgerLogo} 
+            alt="Build Your Own Burger"/>
+        </a>
+    </div>
+);
+
+export default logo;
